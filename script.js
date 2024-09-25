a0266763370e4e06f607f91433729a2b8786108d
Ali-humza <lexoff2023@gmail.com>
2024-09-25 20:26:12 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("hey this is javascript tut.55");
+
+var a = 5;
+var b = 9;
+var c = "ali"
+console.log( a + b );
+
+console.log(typeof a,typeof b,typeof c,);
+
+let x = "hamza";
+let y = 22;
+let z = 3.55;
+let p = undefined;
+let q = null;
+const r = true;
+
+console.log(x,y,z,r,p,q)
+console.log(typeof x,typeof y,typeof z,typeof r,typeof p,typeof q)
+let o = {
+     "name" : "hamza",
+     "Pricings" : "200k",
+     "education" : "matric",
+}
+
+console.log(o)
